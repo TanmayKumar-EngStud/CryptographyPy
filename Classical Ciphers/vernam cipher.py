6350f28c228f55d99d7ab74dca3fbcc8272bb3a6
TanmayKumar-EngStud <tanmay.kumar2019@vitstudent.ac.in>
2021-08-22 15:15:48 +0530

    Adding Vernam Cipher

---
+++ Classical Ciphers/vernam cipher.py
@@ -0,0 +1,51 @@
+from os import system
+system('clear')
+
+plainText = "HelloThere"
+key = "TGHYPTXYUO"
+
+
+print(f"Plain Text is : {plainText}")
+print(f"Key is        : {key}")
+
+def modify(text):
+  text = text.lower()
+  result = []
+  for i in text:
+    result.append(ord(i) - ord('a'))
+  return result
+
+def Encryption(plainText, key):
+  pText = modify(plainText)
+  Key = modify(key)
+  cMatrix = []
+  for i in range (len(pText)):
+    cMatrix.append((pText[i]+Key[i%len(Key)])%26)
+  cText = ""
+  for i in cMatrix:
+    cText += chr(i+ord('A'))
+  return cText
+
+cipherText = Encryption(plainText,key)
+print(f"The cipher text generated is :{cipherText}")
+
+def addInv(num):
+  for i in range(26):
+    if (i+num)%26 == 0:
+      return i
+
+def Decryption(cipherText, key):
+  cText = modify(cipherText)
+  Key = modify(key)
+  pMatrix = []
+
+  for i in range( len(cText)):
+    pMatrix.append((cText[i]+ addInv(Key[i%len(Key)]))%26)
+  pText =""
+  for i in pMatrix:
+    pText += chr(i+ord('A'))
+  return pText
+
+
+GenPlainText = Decryption(cipherText,key)
+print(f"The generated plain text is :{GenPlainText}")
